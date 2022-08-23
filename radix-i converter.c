75ff584c94523a22a70f23672ae58b45c8488242
Richard-UCT <111319313+Richard-UCT@users.noreply.github.com>
2022-08-23 22:38:38 +0200

    another update

--- radix-i converter.c
+++ radix-i converter.c
@@ -4,6 +4,9 @@ int main(){
     printf("Hello World\nEnter name: ");
     char name[20];
     scanf("%s",name);
-    printf("Your name is %s",name);
+    printf("Your name is: %s\nYour age is: ",name);
+    int age;
+    scanf("%d",age);
+    printf("\nYour name is %d",age);
     return 0;
 }
